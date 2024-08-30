3fc3e5b643cf35203781bf3f0ed0efc3a4e13c36
danysanches@msn.com <danysanches@msn.com>
2024-08-30 10:51:39 -0300

    Verficcações com  o BD  rodando

--- src/main/java/com/squad1/locadora/controllers/MotoristaController.java
+++ src/main/java/com/squad1/locadora/controllers/MotoristaController.java
@@ -6,13 +6,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.View;
+
 
 @RestController
 @RequestMapping(value="motoristas")
 public class MotoristaController {
     @Autowired
     private MotoristaRepository motoristaRepository;
-    
+    @Autowired
+    private View error;
+
     @GetMapping
     public List<Motorista> findAll() {
         List<Motorista> result = motoristaRepository.findAll();
@@ -27,11 +31,24 @@ public class MotoristaController {
 
     @PostMapping
     public ResponseEntity<Motorista> criarMotorista(@RequestBody Motorista motorista){
-        if(motoristaRepository.existsById(motorista.getId())){
-            return ResponseEntity.badRequest().body(null);
+
+            if (motoristaRepository.existsById(motorista.getId())) {
+                System.out.println("Id já existe");
+                return ResponseEntity.badRequest().body(null);
+            } else if (motoristaRepository.existsByEmail(motorista.getEmail())) {
+                System.out.println("E-mail já registrado");
+                return ResponseEntity.badRequest().body(null);
+            } else if (motoristaRepository.existsByCpf(motorista.getCpf())) {
+                System.out.println("CPF já existe");
+                return ResponseEntity.badRequest().body(null);
+            } else if (motoristaRepository.existsByNumeroCNH(motorista.getNumeroCNH())) {
+                System.out.println("Número de CNH já existe");
+                return ResponseEntity.badRequest().body(null);
+            }
+
+            Motorista novoMotorista = motoristaRepository.save(motorista);
+            System.out.println("Motorista cadastrado com sucesso");
+            return ResponseEntity.ok(novoMotorista);
         }
 
-        Motorista novoMotorista = motoristaRepository.save(motorista);
-        return ResponseEntity.ok(novoMotorista);
-    }
 }
